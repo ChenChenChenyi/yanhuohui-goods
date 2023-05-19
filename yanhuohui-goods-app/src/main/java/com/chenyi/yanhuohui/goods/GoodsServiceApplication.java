948b6440ec591ea8a76bb29541013c94302f538b
chenyi <614362234@qq.com>
2023-05-19 15:48:41 +0800

    添加mybatisplus的测试用例

--- yanhuohui-goods-app/src/main/java/com/chenyi/yanhuohui/goods/GoodsServiceApplication.java
+++ yanhuohui-goods-app/src/main/java/com/chenyi/yanhuohui/goods/GoodsServiceApplication.java
@@ -2,6 +2,7 @@ package com.chenyi.yanhuohui.goods;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
@@ -12,6 +13,7 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 @Slf4j
 @EnableDubbo
+@MapperScan("com.chenyi.yanhuohui.goods") //设置mapper接口的扫描包
 public class GoodsServiceApplication {
     public static void main(String[] args) {
         Environment env = SpringApplication
---
+++ yanhuohui-goods-app/src/main/java/com/chenyi/yanhuohui/goods/configuration/MybatisPlusConfig.java
@@ -0,0 +1,19 @@
+package com.chenyi.yanhuohui.goods.configuration;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.chenyi.yanhuohui.goods.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+}
